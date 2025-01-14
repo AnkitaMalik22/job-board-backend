a07c7631148521ba8011902bd3c77340daa401e2
AnkitaMalik22 <ankitamalik936@gmail.com>
2025-01-14 18:07:03 +0530

    add connection timeout option for MySQL dialect in Sequelize configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,7 +9,10 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000
+    }
   }
 );
 
